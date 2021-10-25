b99ef62837a84cf1cc3ec556c35b6d91d8668f0b
robsonbittencourt <robson.luizv@gmail.com>
2021-10-24 23:12:19 -0300

    Update scenarios

--- src/main/scala/simulations/BasicSimulation.scala
+++ src/main/scala/simulations/BasicSimulation.scala
@@ -8,7 +8,7 @@ import scala.concurrent.duration._
 
 class BasicSimulation extends Simulation {
 
-  val httpProtocol = http.baseUrl("http://k8s-ecommerc-ingresse-3643cfd7de-358455931.us-west-2.elb.amazonaws.com")
+  val httpProtocol = http.baseUrl("http://k8s-ecommerc-ingresse-3643cfd7de-140063633.us-west-2.elb.amazonaws.com")
 
   val scenario1 = scenario("Create order")
     .exec(
@@ -28,10 +28,29 @@ class BasicSimulation extends Simulation {
       )
 
   val scenario2 = scenario("Get product")
-    .exec(http("Get product").get("/productviewer/product/2"))  
+    .exec(http("Get product").get("/productviewer/product/2"))
+
+  val scenario3 = scenario("Create order Peak")
+    .exec(
+      http("Create order Peak")
+        .post("/order-receiver/")
+        .body(
+          StringBody(
+            """
+            {
+              "clientId": "123",
+              "paymentPlan": "12x",
+              "products": "123, 456, 789"
+            }
+            """
+          )
+        ).asJson
+      )    
 
   setUp(
-    scenario1.inject(constantUsersPerSec(20) during(30 minutes)).protocols(httpProtocol)
+    scenario1.inject(constantUsersPerSec(20) during(10 minutes)).protocols(httpProtocol),
+    scenario2.inject(constantUsersPerSec(20) during(10 minutes)).protocols(httpProtocol),
+    scenario3.inject(nothingFor(5 minutes), constantUsersPerSec(20) during (2 minutes)).protocols(httpProtocol)
   )
 
 }
